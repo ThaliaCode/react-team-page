aaf38d3ec16aad2e6605eb2b17e89dcde9eec336
thaliavynes1 <casamarketthalia@email.com>
2026-06-16 20:10:55 -0500

    Actualizacion de version  desktop

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ function App() {
   return (
     <>
     <main className="flex flex-col justify-center items-center bg-[#ffffff] min-h-screen">
-    <header>
-      <h1 className="font-bold text-2xl mt-9">The creative crew 💡 </h1>
-    </header>
-    <section className="m-9">
-      <h2 className="font-bold text-xl">Who we are</h2>
-      <p>We are team of creatively diverse driven innovative 
+    <header className="md:flex md:justify-between md:items-start md:gap-75">
+      <h1 className="font-bold text-2xl mt-9 md:text-5xl  ">The creative crew  </h1>
+    
+    <section className="m-9 md:w-90 ">
+      <h2 className="font-bold text-xl md:text-3xl ">Who we are</h2>
+      <p className="text-xl">We are team of creatively diverse driven innovative 
         individuals working in various locations  from the world
       </p>
     </section>
-    <section className="mt-1 grid grid-cols-2 gap-x-6 gap-y-12 px-6">
+  
+    </header>
+    <section className="mt-1 grid grid-cols-2 gap-x-6 gap-y-12 px-6  md:grid md:grid-cols-3  md:gap-x-20 md:gap-y-28  md:px-12">
       {team.map((persona, index) => (
         <MemberCard key={index} persona={persona}  index={index}  />
       ))}
